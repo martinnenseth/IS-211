159378bd45b7123448d3f2b931fc477bcb4e704f
Martin Nenseth <martin.nenseth@gmail.com>
2017-03-22 21:15:47 +0100

    ..

--- SailScore/src/sailscore/Main.java
+++ SailScore/src/sailscore/Main.java
@@ -9,14 +9,18 @@ public class Main {
         Regatta konkurranse = new Regatta();
         Boat ronnyboat = new Boat(42, "Dagros");
         Boat ivarboat = new Boat(142, "D11111");
+        Boat xc = new Boat(00, "Uflaks");
+
 
         konkurranse.addCompetitor(ronnyboat);
         konkurranse.addCompetitor(ivarboat);
+        konkurranse.addCompetitor(xc);
 
         konkurranse.prepareNewRace();
 
         konkurranse.registerFinish(ronnyboat);
         konkurranse.registerFinish(ivarboat);
+        konkurranse.registerFinish(xc);
         konkurranse.endRace();
         konkurranse.printResults();
 
--- SailScore/src/sailscore/Regatta.java
+++ SailScore/src/sailscore/Regatta.java
@@ -89,9 +89,8 @@ public class Regatta {
 
     public void printResults(){
         sortResult();
-        competitors.sort(Comparator.comparing(Boat::getScore).reversed());
+        competitors.sort(Comparator.comparing(Boat::getScore));
         printCompetitors();
-
     }
 
 
