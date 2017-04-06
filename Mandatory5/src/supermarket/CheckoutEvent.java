2e20fd1a6e5a77b076e380f33319d1c17f4419f9
Martin Nenseth <martin.nenseth@gmail.com>
2017-04-06 08:57:44 +0200

    works!

--- Mandatory5/src/supermarket/CheckoutEvent.java
+++ Mandatory5/src/supermarket/CheckoutEvent.java
@@ -11,33 +11,17 @@ import eventsim.EventSim;
 public class CheckoutEvent extends Event {
     Customer customer;
 
-    int lol = calcCheckoutTime();
 
-    public CheckoutEvent(Customer customer) {
+    public CheckoutEvent(int time, Customer customer) {
 
         //calcCheckoutTime();
-        super(new CheckoutEvent(customer).calcCheckoutTime());
+        super(time);
         this.customer = customer;
     }
 
     // Simulates the customer standing in queue...
     //If queue is empty, go straight to checkout().
-    public int calcCheckoutTime() {
 
-        if (Checkout.line.size() >= 0) {
-            // The total time all other customers before this one have been waiting is this one's queueWaitTime.
-            for (Customer customer : Checkout.line) {
-                customer.setCheckoutDuration(Checkout.PAY_DURATION + Checkout.PROD_DURATION * customer.getNumProducts());
-                customer.queueWaitDuration = customer.queueWaitDuration + customer.getCheckoutDuration();                 //Get some awesome data for each customer that tells us how long the other customers have been waiting....
-            }
-        }
-        else {
-            customer.setCheckoutDuration(Checkout.PAY_DURATION + Checkout.PROD_DURATION * customer.getNumProducts());
-            customer.queueWaitDuration = 0;}
-
-        customer.setCheckoutTime(customer.queueWaitDuration + super.getTime());
-        return customer.getCheckoutTime();
-    }
 
 
     // Remove that customer from the line Queue.
--- Mandatory5/src/supermarket/PickLineEvent.java
+++ Mandatory5/src/supermarket/PickLineEvent.java
@@ -18,7 +18,22 @@ public class PickLineEvent extends Event {
         this.customer = customer;
     }
 
+    public int calcCheckoutTime() {
 
+        if (Checkout.line.size() >= 0) {
+            // The total time all other customers before this one have been waiting is this one's queueWaitTime.
+            for (Customer customer : Checkout.line) {
+                customer.setCheckoutDuration(Checkout.PAY_DURATION + Checkout.PROD_DURATION * customer.getNumProducts());
+                customer.queueWaitDuration = customer.queueWaitDuration + customer.getCheckoutDuration();                 //Get some awesome data for each customer that tells us how long the other customers have been waiting....
+            }
+        }
+        else {
+            customer.setCheckoutDuration(Checkout.PAY_DURATION + Checkout.PROD_DURATION * customer.getNumProducts());
+            customer.queueWaitDuration = 0;}
+
+        customer.setCheckoutTime(customer.queueWaitDuration + super.getTime());
+        return customer.getCheckoutTime();
+    }
 
     /**
      * A type 3 event do not cause any new events, except if the checkout point is not serving
@@ -29,6 +44,6 @@ public class PickLineEvent extends Event {
     @Override
     public Event happen() {
         Checkout.line.add(customer);
-        return new CheckoutEvent(customer);
+        return new CheckoutEvent(calcCheckoutTime(), customer);
     }
 }
